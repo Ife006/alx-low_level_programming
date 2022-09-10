d0fe2b4582b706d94bf3474df03fa49144af259c
Ife006 <iakinyelepeter@gmail.com>
2022-09-10 12:31:25 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of char: %lu byte(s)\n", sizeof(c));
 	printf("Size of int: %lu byte(s)\n", sizeof(i));
 	printf("Size of long int: %lu byte(s)\n", sizeof(a));
-	printf("Size of long long: int %lu byte(s)\n", sizeof(b));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
