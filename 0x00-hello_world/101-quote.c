0f190af56f8edd6b29a24f23b2ce5cbcf6fed659
Ife006 <iakinyelepeter@gmail.com>
2022-09-11 13:12:18 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
