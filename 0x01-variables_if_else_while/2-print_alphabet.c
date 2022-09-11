dfebf477332a7b22c3d715281c039535ddeb8701
Ife006 <iakinyelepeter@gmail.com>
2022-09-11 16:13:26 -0700

    script that prints the alphabet is in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 if success
+ */
+
+int main(void)
+{
+	char letter
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchcar(letter);
+			letter++;
+	}
+	putchar('\n');
+	return (0);
+}
