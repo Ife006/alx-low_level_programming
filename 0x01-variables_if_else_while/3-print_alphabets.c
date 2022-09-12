6d22c6862f3ac0334ce0236b6fdfb9344e2b4025
Ife006 <iakinyelepeter@gmail.com>
2022-09-12 08:51:42 -0700

    scrip to print lower case , then upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always success if 0
+ */
+int main(void)
+{
+	char lower, upper;
+
+	lower = 'a';
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	upper = 'A';
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+	return (0);
