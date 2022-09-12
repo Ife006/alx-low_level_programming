cde2a66ef4628b7be8be982cd54e2c1444a35b10
Ife006 <iakinyelepeter@gmail.com>
2022-09-12 13:41:34 -0700

    prints lower cases apart from e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char letter
+	char letter;
 
 	letter = 'a';
 	while (letter <= 'z')
 	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
-		letter++
+		letter++;
 	}
 	putchar('\n');
 	return (0);
