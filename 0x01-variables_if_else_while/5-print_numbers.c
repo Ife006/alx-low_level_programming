3f777685028051e0ea1f7370ba5e83de7ea1c2c3
Ife006 <iakinyelepeter@gmail.com>
2022-09-12 13:49:42 -0700

    printing all numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while i < 10;
+	{
+		printf("i\n", i);
+		i++;
+	}
+	return (0);
+}
