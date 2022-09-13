e1698117a9d1c5709c904e6aeeb658131d4ca84e
Ife006 <iakinyelepeter@gmail.com>
2022-09-13 15:43:19 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercasef followed by a new line
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++
+	}
+	_putchar('\n);
+}
