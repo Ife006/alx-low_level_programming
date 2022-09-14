cf030f5dca67a32df9d6642d72a680d706bc63d3
Ife006 <iakinyelepeter@gmail.com>
2022-09-14 08:36:46 -0700

    shopwing the signs of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: character to be checked
+ * Return: 1 if n is greater than zero, 0 if n is equal to zero. -1 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(+);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(0);
+		return (0);
+	}
+	else
+	{
+		putchar(-);
+		return (-1);
+	}
+}
