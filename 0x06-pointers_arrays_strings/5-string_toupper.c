75921b9b11bdc66532437074770e689b5e4c1d81
Ife006 <iakinyelepeter@gmail.com>
2022-09-21 14:18:35 -0700

    changing sstrings to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to be checked
+ * Return: uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = o;
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+		i++;
+	}
+	return (s);
+}
