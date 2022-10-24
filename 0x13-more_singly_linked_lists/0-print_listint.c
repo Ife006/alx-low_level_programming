36caa44e3f820263c2bb427c969ba2dc645566f9
Ife006 <iakinyelepeter@gmail.com>
2022-10-24 15:13:32 -0700

    adjusted the prototype for task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  * @l: a pointer to the head of the listint_t list
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *l)
 {
 	size_t nodes = 0;
 
